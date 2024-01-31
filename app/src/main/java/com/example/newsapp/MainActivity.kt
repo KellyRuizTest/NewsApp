bf4cda0acc30e0a0ff9c803b6cbc5e1a6691d313
KellyR <kelly.ruizh@gmail.com>
2024-01-31 07:04:45 -0800

    first commit

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+//    private val navController by lazy {
+//        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+//        navHostFragment.navController
+//    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // convert this to viewBinding
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.newsNavHostFragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNavigationView.setupWithNavController(navHostFragment!!.findNavController())
+        //
+
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.newsapp.utils
+
+class Constants {
+
+    companion object {
+        val API_KEY: String = "512abca9081e4508ad8085cf5415add9"
+        val BASE_URL: String = "https://newsapi.org/"
+
+    }
+}
